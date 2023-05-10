1ba6b89dbaffebafdb9ba97db01bd970ef2bfe08
Anton Sirkin <gudnos@gmail.com>
2023-05-10 21:40:25 +0300

    First commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,15 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
+    boolean add(User user);
+    List<User> listUsers();
+    void delete(Long id);
+    void update(User user);
+    User findById(Long id);
+    User findByUsername(String userName);
+}
